c040fc101d51408eeb8a4752c2c84cb85451762c
Anderson Andrade <andrade.28me@gmail.com>
2022-12-15 07:02:12 -0300

    hoir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductCreateComponent } from './component/product/product-create/product-create.component';
 import { HomeComponent } from './component/views/home/home.component';
 import { ProductCrudComponent } from './component/views/product-crud/product-crud.component';
 
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: 'products',
     component: ProductCrudComponent,
   },
+  {
+    path:'products/create',
+    component:ProductCreateComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,13 +8,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './component/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from './component/template/footer/footer.component';
 import { NavComponent } from './component/template/nav/nav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HomeComponent } from './component/views/home/home.component';
 import { ProductCrudComponent } from './component/views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './component/product/product-create/product-create.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +27,8 @@ import { ProductCrudComponent } from './component/views/product-crud/product-cru
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
+    RedDirective,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,6 +39,9 @@ import { ProductCrudComponent } from './component/views/product-crud/product-cru
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatButtonModule,
+    FormsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent],
